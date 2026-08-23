46948048ad86c2e7ecca0447cafe488340785e95
Jorge Care <jorgecare@yahoo.es>
2026-08-23 10:26:59 +0200

    fotos seismes

--- works/convertir.py
+++ works/convertir.py
@@ -2,8 +2,10 @@ from PIL import Image
 import os
 
 for archivo in os.listdir():
-    if archivo.lower().endswith(('.webp', '.jpeg', '.webp')):
+    if archivo.lower().endswith((".png", ".jpg", ".jpeg")):
         nombre = os.path.splitext(archivo)[0]
-        img = Image.open(archivo)
-        img.save(f"{nombre}.webp", "WEBP", quality=85)
-        print(f"Convertido: {archivo}")
+
+        with Image.open(archivo) as img:
+            img.save(f"{nombre}.webp", "WEBP", quality=85)
+
+        print(f"Convertido: {archivo} → {nombre}.webp")
